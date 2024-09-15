e1f6f10f271fbf9c34a9d2778c0db5fe518ca230
shamhi <shomakakdela@gmail.com>
2024-09-15 09:52:45 +0300

    🔥 Add x1000 multiplier

--- bot/utils/scripts.py
+++ bot/utils/scripts.py
@@ -2,7 +2,7 @@ import bisect
 
 
 def calculate_spin_multiplier(spins):
-    variables = [1, 2, 3, 5, 10, 50, 150]
+    variables = [1, 2, 3, 5, 10, 50, 150, 1000]
     idx = bisect.bisect_right(variables, spins) - 1
 
     return variables[idx] if idx >= 0 else 1
